4a25a2a3e4eb71dbedf5f1e37b43b8973583cb94
ionutbirlad <birladionut@yahoo.it>
2026-02-20 18:07:07 +0100

    feat: setup msw + init msw + remove unsued asset

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App.tsx";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+async function bootstrap() {
+  if (import.meta.env.DEV) {
+    const { worker } = await import("./msw/browser");
+    await worker.start();
+  }
+
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
+
+void bootstrap();
