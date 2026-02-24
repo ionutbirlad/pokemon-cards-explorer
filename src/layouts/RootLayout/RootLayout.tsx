2ac5e2dc9691ad3fe31b14753b6e52e719874317
ionutbirlad <birladionut@yahoo.it>
2026-02-24 11:49:54 +0100

    style: define basic layout structure

--- src/layouts/RootLayout/RootLayout.tsx
+++ src/layouts/RootLayout/RootLayout.tsx
@@ -1,17 +1,19 @@
-import "./RootLayout.module.scss";
-
 import { Outlet } from "react-router-dom";
 
 import AppFooter from "@/components/AppFooter/AppFooter";
 import AppHeader from "@/components/AppHeader/AppHeader";
 
-export default function AppLayout() {
+import styles from "./RootLayout.module.scss";
+
+export default function RootLayout() {
   return (
-    <div>
+    <div className={styles.layout}>
       <AppHeader />
 
-      <main>
-        <Outlet />
+      <main className={styles.main}>
+        <div className="container">
+          <Outlet />
+        </div>
       </main>
 
       <AppFooter />
