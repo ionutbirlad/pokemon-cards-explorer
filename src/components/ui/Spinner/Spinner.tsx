477af3a6bba5e8b8e2b64719e0e251ffbf0714b1
ionutbirlad <birladionut@yahoo.it>
2026-02-24 18:03:51 +0100

    feat: add spinner component

---
+++ src/components/ui/Spinner/Spinner.tsx
@@ -0,0 +1,19 @@
+import styles from "./Spinner.module.scss";
+import PokeballIcon from "@/assets/icons/pokeball.svg?react";
+
+type SpinnerProps = {
+  size?: number;
+};
+
+export default function Spinner({ size = 64 }: SpinnerProps) {
+  return (
+    <div
+      className={styles.spinner}
+      style={{ width: size, height: size }}
+      role="status"
+      aria-label="Loading"
+    >
+      <PokeballIcon className={styles.spinner__icon} />
+    </div>
+  );
+}
