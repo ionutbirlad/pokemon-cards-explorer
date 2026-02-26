d83c5cbe855e1313597bff6e57351dbb48dbdbaf
ionutbirlad <birladionut@yahoo.it>
2026-02-26 16:53:58 +0100

    feat: create card footer component + apply changes to badge component

--- src/components/ui/Badge/Badge.tsx
+++ src/components/ui/Badge/Badge.tsx
@@ -3,7 +3,7 @@ import styles from "./Badge.module.scss";
 type BadgeVariant = "light" | "dark";
 
 type BadgeProps = {
-  label: string;
+  label?: string;
   icon: React.ReactNode;
   variant?: BadgeVariant;
 };
@@ -11,7 +11,7 @@ type BadgeProps = {
 export default function Badge({ label, icon, variant = "light" }: BadgeProps) {
   return (
     <span className={`${styles.badge} ${styles[`badge--${variant}`]}`}>
-      <span className={styles.badge__label}>{label}</span>
+      {label && <span className={styles.badge__label}>{label}</span>}
       <span className={styles.badge__icon}>{icon}</span>
     </span>
   );
