5c4fb89a3c465f4e4a0801bf0175fa3825d477ea
ionutbirlad <birladionut@yahoo.it>
2026-02-28 18:56:41 +0100

    fix: remove icon from button

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,7 +1,5 @@
 import type { ReactNode } from "react";
 
-import StarIcon from "@/assets/icons/star.svg?react";
-
 import styles from "./Button.module.scss";
 
 type ButtonStatus = "active" | "disabled";
@@ -9,16 +7,10 @@ type ButtonStatus = "active" | "disabled";
 type ButtonProps = {
   children: ReactNode;
   status?: ButtonStatus;
-  withIcon?: boolean;
   onClick?: () => void;
 };
 
-export default function Button({
-  children,
-  status = "active",
-  withIcon = true,
-  onClick,
-}: ButtonProps) {
+export default function Button({ children, status = "active", onClick }: ButtonProps) {
   const isDisabled = status === "disabled";
 
   return (
@@ -27,7 +19,6 @@ export default function Button({
       disabled={isDisabled}
       onClick={onClick}
     >
-      {withIcon && <StarIcon className={styles.button__icon} />}
       <span className={styles.button__label}>{children}</span>
     </button>
   );
