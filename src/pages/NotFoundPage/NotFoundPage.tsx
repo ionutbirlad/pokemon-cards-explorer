2505748a3a378a54ea154abc329abce16769950c
ionutbirlad <birladionut@yahoo.it>
2026-03-01 16:48:41 +0100

    feat: add mvp not found page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
+import notFoundImage from "@/assets/images/general/404.png";
+import Button from "@/components/ui/Button/Button";
+
 import styles from "./NotFoundPage.module.scss";
 
 export default function NotFoundPage() {
-  return <section className={styles.page}>NOT FOUND PAGE HERE</section>;
+  const navigate = useNavigate();
+
+  return (
+    <section className={styles.page}>
+      <img src={notFoundImage} alt="404 - Pagina non trovata" className={styles.image} />
+      <h2 className={styles.title}>Ooops!</h2>
+      <p className={styles.description}>Sembra che ci sia stato un problema, torna all'hompage.</p>
+      <Button onClick={() => navigate("/")} status="active">
+        VAI ALLA HOME
+      </Button>
+    </section>
+  );
 }
