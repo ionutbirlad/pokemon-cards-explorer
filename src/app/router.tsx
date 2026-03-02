7563f8caed39aea2e5a0b07260b27c1f4f3a5682
ionutbirlad <birladionut@yahoo.it>
2026-03-02 18:30:46 +0100

    feat: split app error boundary component + integrate it in router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -4,10 +4,12 @@ import RootLayout from "@/layouts/RootLayout/RootLayout";
 import DeckPage from "@/pages/DeckPage/DeckPage";
 import DetailPage from "@/pages/DetailPage/DetailPage";
 import NotFoundPage from "@/pages/NotFoundPage/NotFoundPage";
+import RouteErrorPage from "@/pages/RouteErrorPage/RouteErrorPage";
 
 export const router = createBrowserRouter([
   {
     element: <RootLayout />,
+    errorElement: <RouteErrorPage />,
     children: [
       { path: "/", element: <Navigate to="/pokemons" replace /> },
       { path: "/pokemons", element: <DeckPage /> },
--- src/components/AppErrorBoundary/AppErrorBoundary.tsx
+++ src/components/AppErrorBoundary/AppErrorBoundary.tsx
@@ -1,18 +1,11 @@
 import type { ReactNode } from "react";
 import { Component } from "react";
 
-import psyduckError from "@/assets/images/general/psyduckError.png";
+import AppErrorFallback from "@/components/AppErrorFallback/AppErrorFallback";
 
-import Button from "../ui/Button/Button";
-import styles from "./AppErrorBoundary.module.scss";
+type Props = { children: ReactNode };
 
-type Props = {
-  children: ReactNode;
-};
-
-type State = {
-  hasError: boolean;
-};
+type State = { hasError: boolean };
 
 export class AppErrorBoundary extends Component<Props, State> {
   state: State = { hasError: false };
@@ -26,36 +19,7 @@ export class AppErrorBoundary extends Component<Props, State> {
   }
 
   render() {
-    if (this.state.hasError) {
-      return (
-        <div className={styles.wrapper}>
-          <div className={styles.wrapper__card}>
-            <div className={styles["wrapper__card-imageWrapper"]}>
-              <img
-                src={psyduckError}
-                alt="Psyduck confuso"
-                className={styles["wrapper__card-imageWrapper-image"]}
-              />
-            </div>
-
-            <h1 className={styles["wrapper__card-title"]}>Ops! Qualcosa è andato storto.</h1>
-
-            <p className={styles["wrapper__card-description"]}>
-              Si è verificato un errore imprevisto durante il caricamento dell'applicazione. Ti
-              consigliamo di aggiornare la pagina e riprovare.
-            </p>
-
-            <Button
-              className={styles["wrapper__card-button"]}
-              onClick={() => window.location.reload()}
-            >
-              Ricarica la pagina 🔄
-            </Button>
-          </div>
-        </div>
-      );
-    }
-
+    if (this.state.hasError) return <AppErrorFallback />;
     return this.props.children;
   }
 }
